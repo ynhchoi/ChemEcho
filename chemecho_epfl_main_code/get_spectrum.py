ecce8fe242da01cefffebd5f882b9573ccc565ae
e-schwen <eulalie.schwendenmann@epfl.ch>
2026-05-13 11:47:27 +0200

    modification of extract_spectrum_data and ir_graph

--- chemecho_epfl_main_code/get_spectrum.py
+++ chemecho_epfl_main_code/get_spectrum.py
@@ -6,6 +6,7 @@ import os
 def extract_spectrum_data(cas:str):
     X = nist.get_compound(cas)
     X.get_ir_spectra()
+    X.ir_specs
     X_IR = X.ir_specs[0]
     jdx_content = X_IR.jdx_text
 
@@ -17,10 +18,26 @@ def extract_spectrum_data(cas:str):
     data = jcamp_readfile(temp_path)
     os.remove(temp_path)
 
-    # extract data
-    wavenumbers = data['x']
-    intensities = data['y']
-    return data
+    data_x = data['x']
+    data_y = data['y']
 
+    if data.get('yunits') == "Absorbance" or data.get('yunits') == "ABSORBANCE" :
+        for i in range(0, len(data_y)):
+            data_y[i] = 10**(-data_y[i])
+    elif data.get('yunits') == "Transmittance" or data.get('yunits') == "TRANSMITTANCE" :
+        pass
+    else :    
+        raise ValueError(f"{X.name} has an IR spectrum with y units not convertible in Transmittance")
+
+    return data_x, data_y
+"""
 print(extract_spectrum_data('74-85-1').get('yunits'))
-print("3")
+X = nist.get_compound('74-85-1')
+X.get_ir_spectra()
+X.ir_specs
+X_IR = X.ir_specs[0]
+jdx_content = X_IR.jdx_text
+print(type(X.ir_specs))
+print(type(jdx_content))
+print(len(X.ir_specs))
+"""
